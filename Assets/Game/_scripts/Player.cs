8358e31ed7d373d7e15c6b67475b1a740c9d8340
Mel Rumsey <melsrumsey@gmail.com>
2020-08-24 10:28:33 -0500

    added player controller and physics

--- Assets/Game/_scripts/Player.cs
+++ Assets/Game/_scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = .7f;
+    private float _jumpHeight = 12f;
+    private float _yVelocity;
+
+     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0 , verticalInput);
+        Vector3 velocity = _speed * direction;
+        velocity.y = _yVelocity;
+        _yVelocity -= _gravity; 
         
+            //To Jump
+        if(_controller.isGrounded == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+            }
+        }
+          
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
