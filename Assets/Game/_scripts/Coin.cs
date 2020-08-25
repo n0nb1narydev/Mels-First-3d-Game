04a7ecbb2b92a92da4a6e2a3482b46a534df3f44
Mel Rumsey <melsrumsey@gmail.com>
2020-08-25 12:40:25 -0500

    shows message to pickup

--- Assets/Game/_scripts/Coin.cs
+++ Assets/Game/_scripts/Coin.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
-    private AudioSource _coinPickupSound;
+    [SerializeField]
+    private AudioClip _coinPickupSound;
     private Player _player;
+    [SerializeField]
+    private Text _pickupText;
 
     private void Start() 
     {
-        _coinPickupSound = GetComponent<AudioSource>();
         _player = GameObject.Find("Player").GetComponent<Player>();
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Player" && _player.hasCoin == false)
+        {
+        _pickupText.gameObject.SetActive(true);
+        }
     }
     private void OnTriggerStay(Collider other) 
     {
         if(other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
         {
-            _player.hasCoin = true;
-            _coinPickupSound.Play();
-            Destroy(this.gameObject, 1f);
+        _player.hasCoin = true;
+        AudioSource.PlayClipAtPoint(_coinPickupSound, transform.position, 1f); //instantiate sound clip (sound, location, volume)
+        Destroy(this.gameObject);
+        _pickupText.gameObject.SetActive(false);
         }
     }
 }
